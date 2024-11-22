b6cf403d65ab5df984fc489125cfed352d8279ab
Adam Žingor <adam@zhincore.eu>
2024-11-22 20:04:18 +0100

    fix: ErrorHandler for linux build

--- code/netpack/ErrorHandler.cpp
+++ code/netpack/ErrorHandler.cpp
@@ -2,12 +2,11 @@
 
 void ErrorHandler::RecordError(absl::string_view filename, int line, int column, absl::string_view message)
 {
-#if TP_PLATFORM_WINDOWS
     std::string fullPath(filename.data(), filename.size());
+#if TP_PLATFORM_WINDOWS
     std::ranges::replace(fullPath, '/', '\\');
+#endif
 
     fullPath = (std::filesystem::current_path() / fullPath).string();
-
-#endif
     throw std::runtime_error(fmt::format("{}({},{}): error 0: {}\n", fullPath, line, column, message));
 }
