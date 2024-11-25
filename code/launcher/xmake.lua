4034cd188fbdd58cc5fb0a25ba3f2863109af4f9
Louis Poirier <contact@louispoirier.io>
2024-11-25 01:09:59 +0100

    ci(Launcher): build and cache with npm
    - restore separate build command (React / Electron)
    - use 'npm ci' instead of 'npm install' only when IS_CI=true (in workflow)
    - update README

--- code/launcher/xmake.lua
+++ code/launcher/xmake.lua
@@ -7,22 +7,36 @@ target("Launcher")
             local stdout, stderr = os.iorunv(cmd, args)
         end
 
+        local function run_install(cmd)
+            if os.getenv("IS_CI") == "true" then
+                run_command(cmd, {"ci"})
+            else
+                run_command(cmd, {"install"})
+            end
+        end
+
         os.cd("code/launcher")
 
         local npm = is_host("windows") and "npm.cmd" or "npm"
 
         -- Run npm commands in the root directory
-        run_command(npm, {"install"})
-        run_command(npm, {"run", "build:all"})
+        run_install(npm)
+        run_command(npm, {"run", "build"})
 
         local electron_dir = path.join(os.curdir(), "electron")
         if not os.isdir(electron_dir) then
             raise("Electron directory not found: " .. electron_dir)
         end
 
+        os.cd(electron_dir)
+
+        -- Run npm commands in the electron directory
+        run_install(npm)
+        run_command(npm, {"run", "build:electron"})
+
         -- Copy produced files
         local src = path.join(electron_dir, "out", "win-unpacked")
-        local dst = path.join("..", "..", target:installdir("launcher"))
+        local dst = path.join("..", "..", "..", target:installdir("launcher"))
         print(string.format("Copying from %s to %s", src, dst))
         if not os.isdir(src) then
             raise("Source directory not found: " .. src)
