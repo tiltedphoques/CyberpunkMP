e30749e82acf03cd1163096501a4aabfc1194062
Louis Poirier <contact@louispoirier.io>
2024-12-09 03:04:48 +0100

    feat(EmoteSystem): add React widget
    - in '...System\Admin\'
    - minimal project with React and Vite
    - entrypoint to widget.tsx -> widget.mjs
    
    WIP

---
+++ code/server/scripting/EmoteSystem/Admin/vite.config.js
@@ -0,0 +1,28 @@
+﻿import {defineConfig} from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+    build: {
+        target: 'modules',
+        lib: {
+            name: 'emote',
+            entry: './widget.tsx',
+            formats: ['es'],
+            fileName: 'widget',
+        },
+        rollupOptions: {
+            // NOTE: Exclude dependencies already declared in Admin webapp.
+            //       This will reduce the bundle of your widget with only the
+            //       minimum required.
+            external: [
+                '@emotion/react',
+                '@emotion/styled',
+                '@mui/icons-material',
+                '@mui/material',
+                'react',
+                'react-dom'
+            ],
+        },
+    },
+    plugins: [react()],
+});
---
+++ code/server/scripting/EmoteSystem/Admin/widget.tsx
@@ -0,0 +1,64 @@
+﻿import {Alert, Card, CardActionArea, Tooltip, Typography} from "@mui/material";
+import {useEffect, useState} from "react";
+
+export default function Widget() {
+    const [username, setUsername] = useState<string>('');
+    const [emote, setEmote] = useState<string>('');
+
+    useEffect(() => handleRefresh(), [username, emote]);
+
+    // NOTE: start an interval to automatically fetch last emote every X s/m/h.
+
+    // @ts-ignore
+    const getLastEmote = async () => {
+        const response = await fetch('/api/v1/plugins/emote/');
+
+        if (!response.ok) {
+            return;
+        }
+        const json = await response.json();
+
+        return {
+            username: json.Username ?? '',
+            emote: json.Emote ?? '',
+        };
+    };
+
+    const handleRefresh = () => {
+        // @ts-ignore
+        async function request() {
+            const data = await getLastEmote();
+
+            if (lock) {
+                return;
+            }
+            setUsername(data.username);
+            setEmote(data.emote);
+        }
+
+        let lock: boolean = false;
+
+        request();
+        return () => {
+            lock = true;
+        };
+    }
+
+    const isEmpty = () => username.length === 0 && emote.length === 0;
+
+    return (
+        <Tooltip title="Click to refresh">
+            <Card>
+                <CardActionArea onClick={handleRefresh}>
+                    {isEmpty() ?
+                        <Alert severity="info">
+                            No emote played for now...
+                        </Alert> :
+                        <Typography variant="body2" color="text.secondary">
+                            Last emote was {emote} by {username}.
+                        </Typography>}
+                </CardActionArea>
+            </Card>
+        </Tooltip>
+    );
+}
