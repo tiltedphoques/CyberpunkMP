d8b9c3d25545fb5060dac7a0fa448ccf5dc9524f
Louis Poirier <contact@louispoirier.io>
2024-12-10 00:25:06 +0100

    feat(Admin): dynamically load plugin's widget
    - export widget using UMD and external dependencies.
    - import widget using SystemJS.
    - render as-is in Dashboard page for now.
    
    TODO:
    - provide a docker layout to organize/resize widgets.
    - improve development workflow.
    - test production workflow.

--- code/admin/src/main.tsx
+++ code/admin/src/main.tsx
@@ -1,8 +1,14 @@
-import {StrictMode} from 'react';
+import React, {StrictMode} from 'react';
+import * as MaterialUI from '@mui/material';
 import {createRoot} from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
 import {BrowserRouter} from "react-router";
+import 'systemjs';
+
+window.React = React;
+// @ts-expect-error should declare in global
+window.MaterialUI = MaterialUI;
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
